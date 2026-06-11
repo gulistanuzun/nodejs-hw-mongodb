9641094d50855a3e17716b3d373e97e08320a6d1
gulistanuzun <uzun.gulistan1@gmail.com>
2026-06-11 13:20:45 +0300

    fix: remove router code from services

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,12 +1,7 @@
 import { ContactModel } from '../db/models/Contact.js';
-import router from '../routers/contacts.js';
 
 export const getAllContacts = () => ContactModel.find();
 export const getContactById = (id) => ContactModel.findById(id);
 export const createContact = (data) => ContactModel.create(data);
-
 export const updateContact = (id, data) => ContactModel.findByIdAndUpdate(id, data, { new: true });
-
-router.patch('/:contactId', ctrlWrapper(updateContactController));
-
 export const deleteContact = (id) => ContactModel.findByIdAndDelete(id);
